f951eaf4668f4d06293c19c27e3f311f78d5043e
Bryan Yj Actub <nirvy.actub@gmail.com>
2024-03-08 22:18:01 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native"
+
+
+const App = () => {
+
+
+  return(
+    <View style={styles.container}>
+        <Text>Coffee App !</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+
+});
+
+export default App;
